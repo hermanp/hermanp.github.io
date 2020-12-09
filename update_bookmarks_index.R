fe3f3126b0474c2f6de16c859f1c02224e000421
hermanp <hermanp@users.noreply.github.com>
2020-12-09 10:27:11 +0000

    deploy: hermanp/website@91c642594f84f2e95847c7998f74a794fc1d5104

--- update_bookmarks_index.R
+++ update_bookmarks_index.R
@@ -1,4 +1,3 @@
-library(jsonlite)
 source("static/generate_md.R")
 
 # Generate a markdon file for the site.
@@ -7,12 +6,6 @@ source("static/generate_md.R")
 #                        "33c21c88dad35145e2792b6258ede9c882c580ec/",
 #                        "bookmarks-example.json")))
 
-# Prettify the bookmarks JSON file to make it version controllable.
-# Overwrite the original. useBytes = T is important!
-jsonfile <- file.path("static", "bookmarks-2020-12-08.json")
-bmarks <- prettify(toJSON(fromJSON(txt = jsonfile)), indent = 2)
-writeLines(bmarks_json_lite, "static/bookmarks.json", useBytes = T)
-
 index_md <- readLines("content/en/resources/bookmarks/index.md",
                       encoding = "UTF-8")
 end_of_yaml <- which(grepl("---", index_md))[2]
@@ -20,8 +13,8 @@ front_matter <- index_md[seq.int(from = 1, to = end_of_yaml, by = 1L)]
 
 bookmarks_md <- paste0(paste0(front_matter, collapse = "\n"),
                        "\n",
-                       generate_md(jsonfile = jsonfile,
-                                   foricondownload = F))
+                       generate_md(paste0("static/",
+                                          "bookmarks.json")))
 
 writeLines(text = bookmarks_md,
            con = "content/en/resources/bookmarks/index.md",
@@ -30,4 +23,3 @@ writeLines(text = bookmarks_md,
 # Download favicons for display.
 # https://superuser.com/questions/157925/how-to-download-favicon-from-website
 # Dismissed.
-# download_favicons(jsonfile = jsonfile)
